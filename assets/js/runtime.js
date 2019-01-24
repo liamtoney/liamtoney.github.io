f673ddf9108e83138f44f18b3be066321d8fb7a3
Liam Toney <toney.liam@gmail.com>
2019-01-24 00:00:25 -0900

    Run atom-beautify

--- assets/js/runtime.js
+++ assets/js/runtime.js
@@ -6,25 +6,25 @@ Authored by J.H. van der Schee
 
 function external_new_window() // open external links in a new window
 {
-    for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
-        var b = c[a];
-        b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
-    }
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+    var b = c[a];
+    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+  }
 }
 
 function pdf_new_window() // open PDF links in a new window
 {
-    if (!document.getElementsByTagName) return false;
-    var links = document.getElementsByTagName("a");
-    for (var eleLink = 0; eleLink < links.length; eleLink++) {
-        if (links[eleLink].href.indexOf(".pdf") !== -1) {
-            links[eleLink].onclick =
-                function() {
-                    window.open(this.href);
-                    return false;
-                }
+  if (!document.getElementsByTagName) return false;
+  var links = document.getElementsByTagName("a");
+  for (var eleLink = 0; eleLink < links.length; eleLink++) {
+    if (links[eleLink].href.indexOf(".pdf") !== -1) {
+      links[eleLink].onclick =
+        function() {
+          window.open(this.href);
+          return false;
         }
     }
+  }
 }
 
 external_new_window();
@@ -36,15 +36,15 @@ https://stackoverflow.com/a/19531814
 */
 
 function wrap_tables() {
-    for (var tbls = document.getElementsByTagName("table"), t = 0; t < tbls.length; t++) {
-        var tbl = tbls[t]
-        var parent = tbl.parentNode;
-        var wrapper = document.createElement("div");
-        wrapper.style.overflowX = "auto";
-        wrapper.style.webkitOverflowScrolling = "touch";
-        parent.insertBefore(wrapper, tbl);
-        wrapper.appendChild(tbl);
-    }
+  for (var tbls = document.getElementsByTagName("table"), t = 0; t < tbls.length; t++) {
+    var tbl = tbls[t]
+    var parent = tbl.parentNode;
+    var wrapper = document.createElement("div");
+    wrapper.style.overflowX = "auto";
+    wrapper.style.webkitOverflowScrolling = "touch";
+    parent.insertBefore(wrapper, tbl);
+    wrapper.appendChild(tbl);
+  }
 }
 
 wrap_tables();
